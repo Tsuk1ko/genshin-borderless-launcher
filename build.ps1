424513e21c76ef81e1624bb357526cabc2e8d6a8
神代綺凜 <i@loli.best>
2020-10-17 12:48:11 +0800

    feat: build with version automatically

--- build.ps1
+++ build.ps1
@@ -1,2 +1,3 @@
+$version = (git rev-list --tags --max-count=1 | git describe --tags) -replace 'v', ''
 Remove-Item genshin-launcher.exe -Force -ErrorAction SilentlyContinue
-ps2exe main.ps1 genshin-launcher.exe -noConsole -iconFile logo.ico -title "Genshin Impact Launcher" -product "Genshin Impact Launcher" -copyright "Tsuk1ko/genshin-launcher" -requireAdmin -noOutput -version $args[0]
+ps2exe main.ps1 genshin-launcher.exe -noConsole -iconFile logo.ico -title "Genshin Impact Launcher" -product "Genshin Impact Launcher" -copyright "Tsuk1ko/genshin-launcher" -requireAdmin -noOutput -version $version
